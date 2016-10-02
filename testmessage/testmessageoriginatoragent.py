58bcee70e9e7314b62d5dcd28699cc36768aa14f
Jordan87M <jmm72@case.edu>
2016-10-02 07:41:38 -0400

    now supports 10 message types. also fixed some errors

--- testmessage/testmessageoriginatoragent.py
+++ testmessage/testmessageoriginatoragent.py
@@ -37,18 +37,54 @@ class TestMessageOriginatorAgent(Agent):
         self.EventID += 1
         now = datetime.utcnow().isoformat(' ') + 'Z'
         
-        mesdict = {"message_target": "all",
-                   "message_subject": "new_event",
-                   "event_uid": str(self.EventID),
-                   "event_name": "shed",
-                   "event_duration": "4"
-                   }
+        mesdict = self.supplyMessageDict((self.EventID % 10) + 1)
+        
+        mesdict["message_target"] = "all"
+        mesdict["message_subject"] = "new_event"
+        mesdict["event_uid"] = str(self.EventID)
         
         message = json.dumps(mesdict)
         print(message)
         
         self.vip.pubsub.publish('pubsub','CTAevent',{}, message)
         
+    def supplyMessageDict(self,choice):
+        if choice == 1:
+            mesdict = {"event_name": "shed",
+                       "event_duration": "4",
+                       "message_type": "shed"}
+        elif choice == 2:
+            mesdict = {"event_name": "normal",
+                       "message_type": "normal"}
+        elif choice == 3:
+            mesdict = {"event_name": "critical_peak",
+                       "event_duration": "134",
+                       "message_type": "critical_peak"}
+        elif choice == 4:
+            mesdict = {"event_name": "grid_emergency",
+                       "event_duration": "23",
+                       "message_type": "grid_emergency"}
+        elif choice == 5:
+            mesdict = {"cur_price": "5.7",
+                       "message_type": "cur_price"}
+        elif choice == 6:
+            mesdict = {"next_price": "3.4",
+                      "message_type": "next_price"}
+        elif choice == 7:
+            mesdict = {"time_remaining": "24",
+                      "message_type": "time_remaining"}
+        elif choice == 8:
+            mesdict = {"day": "3",
+                      "hour": "12",
+                      "message_type": "time_sync"}
+        elif choice == 9:
+            mesdict = {"message_type": "query_op_state"}
+        elif choice == 10:
+            mesdict = {"message_type": "info_request"}
+            
+        return mesdict 
+        
+
 def main(argv=sys.argv):
     '''main method called by the eggsecutable'''
     try:
